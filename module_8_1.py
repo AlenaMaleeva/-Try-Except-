cc286c17880055c588e5ce75daaf2e44a07c1219
Alena <89519703610alena@gmail.com>
2024-11-09 21:39:13 +0500

    Add files via upload

--- module_8_1.py
+++ module_8_1.py
@@ -1,9 +1,13 @@
 def add_everything_up(a, b):
-    if (isinstance(a, int) or isinstance(a, float)) and (isinstance(b, int) or isinstance(b, float)):
-        return a + b
-    elif isinstance(a, str) and isinstance(b, str):
-        return a + b
-    else: return str(a) + str(b)
+    try:
+        if (isinstance(a, int) or isinstance(a, float)) and (isinstance(b, int) or isinstance(b, float)):
+            return a + b
+        elif isinstance(a, str) and isinstance(b, str):
+            return a + b
+        else:
+            raise TypeError
+    except TypeError:
+        return str(a) + str(b)
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
